2a2913abcf4d20be6f66535adbadb02bc3423c52
Mario Neuhold <mario.neuhold@proton.me>
2025-03-20 22:30:26 +0100

    chore: add ui pro, setup prettier and format everything

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<UApp>
+		<NuxtRouteAnnouncer />
+		<NuxtWelcome />
+	</UApp>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+	plugins: {
+		prettier: prettierPlugin,
+	},
+	rules: {
+		// ...prettierPlugin.configs.recommended.rules,
+		...eslintConfigPrettier.rules,
+		...eslintPluginPrettierRecommended.rules,
+	},
+})
